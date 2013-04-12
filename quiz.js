ca62b86e54a49dea0a7a942bfb4d25c588eba5c2
unknown <alec@newmanhome.org>
2013-04-12 07:54:42 -0600

    Added hide fragment to URL.

--- quiz.js
+++ quiz.js
@@ -103,8 +103,18 @@ function arrangeQuestions (questions, facets) {
   return result;
 }
 
+function handleHash () {
+  var hash = window.location.hash.substring(1);
+  
+  if (hash == 'hide') {
+    $('#all-results').toggle();
+  }
+}
+
 function begin () {
   var facets = {};
+  
+  handleHash();
   
   QUESTIONS = QUESTION_SET.slice(0);
   
